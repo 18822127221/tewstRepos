54bedce695e59586fe324d9684082aa7a2885bd2
houyue <2664365133@qq.com>
2023-03-04 17:47:44 +0800

    11

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-// 引入一键图表脚本
-THING.Utils.dynamicLoad(['/static/ScenePreview/chart/PreviewChartControl.js'], function () {
-    // 创建图表
-    new PreviewChartControl({
-        url: '/api/chart/62c39bdbee63ce05082e4a60',
-        tag: '1',
-        isLoad: true,  // 加载页，默认为true
-        isApplyBackground: false,  // 是否应用图表背景，启用该参数会替换app.background的值，默认false
-        autoResize: {  // 图表自适应
-            enable: false,  // 自适应图表分辨率，默认为false
-            orgin: 'center center'  // 自适应的基准点，第一个参数取值为:left、center、right，第二个参数取值为:top、center、bottom，默认值为“center center”
-        },
-        complete: function () {
-            console.log('1vvvv')
-            console.log("2222222222222")
-        }
-    });
-})
+
+
+// 离线开发引入园区场景
+var app = new THING.App({
+    url: '/api/scene/20220927164609809107533'  // 场景地址
+});
